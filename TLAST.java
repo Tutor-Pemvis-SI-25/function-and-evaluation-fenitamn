a91c42a328c5e2f5d1dec64eff50ef297bf07eff
fenitasimbolon3344 <fenitasimbolon@gmail.com>
2025-11-18 21:16:57 +0700

    FENITA

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,30 @@
-//Nama - NIM
+//Fenita Simbolon - 12S25017
 
+import java.util.*;
+import java.lang.Math;
+
+public class TFUNC {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] namatugas = new String[10], nim = new String[10], matkul = new String[10], dosen = new String[10], deadline = new String[10];
+        int i;
+
+        for (i = 0; i <= 9; i++) {
+            namatugas[i] = input.nextLine();
+            nim[i] = input.nextLine();
+            matkul[i] = input.nextLine();
+        }
+        addtask(namatugas, nim, matkul, dosen, deadline);
+    }
+    
+    public static void addtask(String namatugas, String nim, String matkul, String dosen, String deadline) {
+        namatugas = input.nextLine();
+        nim = input.nextLine();
+        matkul = input.nextLine();
+        dosen = input.nextLine();
+        deadline = input.nextLine();
+        String namatugas, nim, matkul, dosen, deadline;
+
+    }
+}
